16e4294b655196f83e15717bb76c87eff1f78781
adnan <adnann3077@gmail.com>
2024-04-19 18:14:54 +0530

    Build basic version

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          950: '#97fe33',
+        },
+        secondary: {
+          950: '#104534'
+        },
+        black: {
+          950: '#1a1a1a'
+        },
+        red: {
+          950: "#ffead4"
+        },
+        gray: {
+          950: "#f1f2ed"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
