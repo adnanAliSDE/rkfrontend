bb7339ef50263d2c0e3edad0a7916a6f131d0f3d
adnan <adnann3077@gmail.com>
2024-04-22 12:52:48 +0530

    Completed Blog Page

---
+++ static/scripts/blog.js
@@ -0,0 +1,15 @@
+const slider = document.getElementById('slider-1')
+const mainPost = document.getElementById('main-post')
+
+const mainPostHeight = mainPost.scrollHeight
+let scrollPercentage;
+document.addEventListener('scroll', (e) => {
+    let heightDiff = mainPostHeight - window.scrollY
+    console.log("Height Diff: ", heightDiff)
+
+    if (heightDiff >= 0) {
+        scrollPercentage = 100 - (heightDiff / mainPostHeight) * 100
+        console.log(scrollPercentage)
+        slider.style.width = scrollPercentage + "%"
+    }
+})
