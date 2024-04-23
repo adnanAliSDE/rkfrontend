77cb383c1e254f9f34add99a59d7990067a2f0f5
adnan <adnann3077@gmail.com>
2024-04-23 10:54:30 +0530

    Completed contact page

--- static/scripts/blog.js
+++ static/scripts/blog.js
@@ -6,10 +6,11 @@ let scrollPercentage;
 document.addEventListener('scroll', (e) => {
     let heightDiff = mainPostHeight - window.scrollY
     console.log("Height Diff: ", heightDiff)
-
     if (heightDiff >= 0) {
         scrollPercentage = 100 - (heightDiff / mainPostHeight) * 100
         console.log(scrollPercentage)
         slider.style.width = scrollPercentage + "%"
+    } else {
+        slider.style.width = 100 + "%"
     }
 })
